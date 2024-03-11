452ac78db98e2ee95effc4975bc733092375ac54
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-03-11 22:53:05 +0530

    Add admin post delete route

--- server/src/api/v1/routes/admin.js
+++ server/src/api/v1/routes/admin.js
@@ -3,12 +3,13 @@ import express from "express"
 // middlewares
 import { verifyJWT } from "../../../middlewares/index.js"
 
-import { adminLogin, getAllUsers, deleteUser } from "../controllers/admin.js"
+import { adminLogin, getAllUsers, deleteUser, deleteBlog } from "../controllers/admin.js"
 
 const router = express.Router()
 
 router.post("/login", adminLogin)
 router.get("/all-users", verifyJWT, getAllUsers)
 router.post("/delete-user", verifyJWT, deleteUser)
+router.post("/delete-blog", verifyJWT, deleteBlog)
 
 export default router
